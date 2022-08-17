2568e811fe4ec4bc8d2336dc9e553002521c4f0f
binarygit <inscription4@outlook.com>
2022-08-17 14:02:27 +0545

    Refactor and display the returned data with the total time

--- app/javascript/controllers/network_test_controller.js
+++ app/javascript/controllers/network_test_controller.js
@@ -1,18 +1,19 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static values = { url: String }
-  static targets = ["message"]
+  static values = { url: String };
+  static targets = ["message"];
 
   test() {
-    let start = new Date()
+    let start = new Date();
 
     fetch(this.urlValue)
-      .then(response => response.json())
-      .then(result => console.log(result))
-    
-    let end = new Date()
-
-    this.messageTarget.textContent = `Operation took ${end.getTime() - start.getTime()} msec`
+      .then((response) => response.json())
+      .then((responseData) => {
+        let end = new Date();
+        this.messageTarget.textContent = `The response data is ${responseData} and the operation took ${
+          end.getTime() - start.getTime()
+        } msec`;
+      });
   }
 }
