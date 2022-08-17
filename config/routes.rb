0a53d81781a5b26810e301e8e8b6e932cc778fd5
binarygit <inscription4@outlook.com>
2022-08-17 15:50:36 +0545

    Create users and display all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root 'users#index'
   get '/home', to: 'static_pages#home'
   get '/static_pages/network_test', to: 'static_pages#network_test'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
